4b484e98db29f4ed4e0b4fca0558f3751875e218
Soufiane AMMY DRISS <ammydriss18@gmail.com>
2020-08-10 13:31:17 +0100

    End

--- log_analyser/application/views.py
+++ log_analyser/application/views.py
@@ -15,7 +15,7 @@ def newLog(request):
         fs = FileSystemStorage()
         filename = fs.save(file_url.name, file_url)
         uploaded_file_url = fs.url(filename)
-        #request.session['uploaded_file_url'] = uploaded_file_url
+        request.session['uploaded_file_url'] = uploaded_file_url
         content = models.read_file(uploaded_file_url)
         context = {'content': content}
     return render(request, 'application/logs.html', context)
@@ -24,6 +24,7 @@ def filter(request):
     if request.method == "POST":
         key = request.POST['key']
         uploaded_file_url = request.session['uploaded_file_url']
+        print(uploaded_file_url)
         content = models.read_file_filter(uploaded_file_url, key)
         context = {'content': content}
     return render(request, 'application/logs.html', context)
